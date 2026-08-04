b25bce0897a0fc79e3e0de99bdcb8785d569c619
fuzeonspotify <83621025+fuzeonspotify@users.noreply.github.com>
2026-08-04 00:39:50 -0700

    Spawn above the map-wide environment showcase area

--- AetherFlight_Cinematic_v5_WITH_BUILD_FILES/Source/AetherFlight/CinematicFlightPawn.h
+++ AetherFlight_Cinematic_v5_WITH_BUILD_FILES/Source/AetherFlight/CinematicFlightPawn.h
@@ -89,7 +89,16 @@ protected:
     UCameraComponent* CinematicCamera;
 
     UPROPERTY(EditAnywhere, Category = "Flight|Spawn", meta = (ClampMin = "1000.0", ClampMax = "50000.0"))
-    float SpawnAltitudeFeet = 20000.0f;
+    float SpawnAltitudeFeet = 9000.0f;
+
+    UPROPERTY(EditAnywhere, Category = "Flight|Spawn")
+    bool bUseEnvironmentShowcaseSpawn = true;
+
+    UPROPERTY(EditAnywhere, Category = "Flight|Spawn")
+    FVector2D EnvironmentShowcaseCenter = FVector2D(-400000.0f, 400000.0f);
+
+    UPROPERTY(EditAnywhere, Category = "Flight|Spawn", meta = (ClampMin = "50000.0", ClampMax = "500000.0"))
+    float EnvironmentShowcaseApproachDistanceCm = 180000.0f;
 
     UPROPERTY(EditAnywhere, Category = "Flight|Airframe", meta = (ClampMin = "1000.0"))
     float AircraftMassKg = 8500.0f;
